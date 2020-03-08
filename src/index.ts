3ab54bec42e54393a80ed047d6e0fb52c4e3be60
Alex Redwood <aredwood@users.noreply.github.com>
2020-03-08 21:35:01 +1100

    updated cloud build

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,37 @@
 import getCoronavirusSummary from "./lib/getCoronavirusSummary";
-
+import {Request,Response} from "express";
+import logger from "./logger"
+import lodash from "lodash"
 // https://cloud.google.com/functions/docs/bestpractices/tips#use_global_variables_to_reuse_objects_in_future_invocations
 let cache = {};
+let cacheTime = 0;
 
-
-const handler = async () => {
+const handler = async (req:Request,res:Response) => {
 
     let response;
 
-    if(!cache){
+    // if the cache is 15 seconds old
+    const oldCache = new Date().getTime() - cacheTime > 15 * 1000;
+
+    if(lodash.isEmpty(cache) || oldCache){
         const summary = await getCoronavirusSummary();
 
+        // compute the response
         response = {
             note: "data provided by worldometers.info/coronavirus/",
             summary
         }
+        
+        // save it to cache
+        cache = response;
     }
     else{
         response = cache;
     }
 
-    return response
+    // send it
+    res.send(response);
 }
+
 
 export default handler;
