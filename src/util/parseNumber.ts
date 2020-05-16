b47fc760aebc4e937932d1c057e5ee038e8a25b7
Alex Redwood <aredwood@users.noreply.github.com>
2020-05-16 21:04:38 +1000

    resolved lionting issues on number util

--- src/util/parseNumber.ts
+++ src/util/parseNumber.ts
@@ -3,7 +3,7 @@
  * this takes that, and returns an actual number
  * @param {string} roughNumber - with spaces, and commas
  */
-const parseNumber = (roughNumber: string,t:number = 0): number => {
+const parseNumber = (roughNumber: string): number => {
     // splitting and joining is actually quicker
     try{
         return Number(roughNumber.split(",").join("").trim());
