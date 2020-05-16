2c327f8835a96986262ed7eeb941e9c9dea56be5
Alex Redwood <aredwood@users.noreply.github.com>
2020-05-17 03:58:10 +1000

    validation testing

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 // import getCoronavirusSummary from "./lib/getCoronavirusSummary";
 import {Request,Response} from "express";
 import lodash from "lodash"
+import Joi from '@hapi/joi';
 import getCoronavirusCountryBreakdown from "./lib/getCoronavirusCountryBreakdown";
 import getCoronavirusSummary from "./lib/getCoronavirusSummary";
 // https://cloud.google.com/functions/docs/bestpractices/tips#use_global_variables_to_reuse_objects_in_future_invocations
@@ -15,7 +16,48 @@ const handler = async (req: Request,res: Response): Promise<void> => {
     const oldCache = new Date().getTime() - cacheTime > 15 * 1000;
 
     if(lodash.isEmpty(cache) || oldCache){
+
+
         const [summary,breakdown] = await Promise.all([getCoronavirusSummary(),getCoronavirusCountryBreakdown()]);
+
+        const summaryValidationSchema = Joi.object({
+            deaths: Joi.number().positive(),
+            cases: Joi.number().positive(),
+            newCases: Joi.number().positive(),
+            newDeaths: Joi.number().positive(),
+            recovered: Joi.number().positive(),
+            activeCases: Joi.number().positive(),
+            seriousCases: Joi.number().positive(),
+            casesPer1M: Joi.number().positive()
+        })
+
+        const countryValidationSchema = Joi.object({
+            country: Joi.string().min(1),
+            totalCases: Joi.number().positive(),
+            newCases: Joi.number().positive(),
+            totalDeaths: Joi.number().positive(),
+            newDeaths: Joi.number().positive(),
+            totalRecovered: Joi.number().positive(),
+            activeCases: Joi.number().positive(),
+            seriousCases: Joi.number().positive(),
+            casesPer1M: Joi.number().positive()
+        })
+
+        const breakdownValidationSchema = Joi.array().items(countryValidationSchema);
+
+        try{
+            await Promise.all(
+                [
+                    breakdownValidationSchema.validateAsync(breakdown),
+                    summaryValidationSchema.validateAsync(summary)
+                ]
+            )
+        }
+        catch(err){
+            console.error(err);
+            //TODO report why the validation failed.
+            throw new Error("VALIDATION FAILED");
+        }
 
         // shape the response
         response = {
@@ -23,9 +65,7 @@ const handler = async (req: Request,res: Response): Promise<void> => {
             by:"github.com/aredwood",
             summary,
             breakdown
-
         }
-        
         // save it to cache
         cache = response;
     }
